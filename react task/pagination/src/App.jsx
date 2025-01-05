f6cf5b0fc30fdd8fb6fc374c0590749d96933cdf
Sohel Shaikh <sohelshaikh30611@gmail.com>
2025-01-05 10:38:47 +0530

    pagination is completed

--- react task/pagination/src/App.jsx
+++ react task/pagination/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import { useState } from "react";
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const productsData = async () => {
+    const res = await fetch("https://dummyjson.com/products");
+    const data = await res.json();
+    console.table(data);
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+  };
+  console.log("Prodcuts", products);
+  console.table(products);
 
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    productsData();
+  }, []);
 
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= products.length / 6 &&
+      selectedPage !== page
+    ) {
+      setPage(selectedPage);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Prodcuts</h1>
+      {products.length > 0 && (
+        <div className="products">
+          {products.slice(page * 6 - 6, page * 6).map((prod) => {
+            return (
+              <span key={prod.id} className="products__single">
+                <h3>{prod.title}</h3>
+                <img
+                  src={prod.thumbnail}
+                  alt={prod.tilte}
+                  style={{ width: 220 }}
+                />
+              </span>
+            );
+          })}
+        </div>
+      )}
+      {products.length > 0 && (
+        <div className="pagination">
+          <span
+            className={page > 1 ? "" : "pagination__disable"}
+            onClick={() => selectPageHandler(page - 1)}
+          >
+            ⬅️
+          </span>
+          {[...Array(products.length / 6)].map((_, i) => {
+            return (
+              <span
+                className={page === i + 1 ? "pagination__selected" : ""}
+                key={i}
+                onClick={() => selectPageHandler(i + 1)}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+          <span
+            onClick={() => selectPageHandler(page + 1)}
+            className={
+              page === products.length / 6 ? "pagination__disable" : ""
+            }
+          >
+            ➡️
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
